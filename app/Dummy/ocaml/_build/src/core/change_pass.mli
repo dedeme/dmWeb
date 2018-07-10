3ca1523f0a5704641f876dfd13daafac8269f7e9
Deme <deme@debian.es>
2018-07-10 13:39:44 +0200

    Modified .gitignore

--- app/Dummy/ocaml/_build/src/core/change_pass.mli
+++
@@ -1,6 +0,0 @@
-(* Copyright 10-07-2018 ºDeme
-   GNU General Public License - V3 <http://www.gnu.org/licenses/>
-*)
-
-val process : Cgi.t -> Json.t Dic.t -> Cgi.rp
-(** [process cgi rq] processes change passoword page *)
