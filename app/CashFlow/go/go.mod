75de459a6d079817442736f7ffd8b6a931ddecf4
deme <deme@debian.es>
2023-09-12 13:15:46 +0200

    Remove CashFlow

--- app/CashFlow/go/go.mod
+++
@@ -1,6 +0,0 @@
-module github.com/dedeme/CashFlow
-
-go 1.18
-
-// v2023.01
-require github.com/dedeme/ktlib v0.0.0-20230117124342-fcf8ddebad7f
--- app/CashFlow/hx/src/pgs/budget/fixProblem/ProblemEditor.hx
+++
@@ -1,9 +0,0 @@
-// Copyright 30-Aug-2021 ºDeme
-// GNU General Public License - V3 <http://www.gnu.org/licenses/>
-
-package pgs.budget.fixProblem;
-
-interface ProblemEditor {
-  public function deactivate (): Void;
-  public function updateAccount (acc: String): Void;
-}
